83f2a99bd338297c650a8fc2ea9d42cbff4a66f6
Sonja01 <75359906+Sonja01@users.noreply.github.com>
2023-11-13 14:09:48 +0100

    Update pseudocode_bees.py

--- pseudocode_bees.py
+++ pseudocode_bees.py
@@ -4,9 +4,11 @@
 # environment: Vad finns i närheten? (lista)
 
 class Bee:
-  def __init__(environment):
+  def __init__(self, environment):
     self.position() = [0,0,0] # x, y, angle
-    
+    self.fed() = 0            # (float 0-1)? (0 = very hungry)
+    self.visited() = []       # visited flowers in order
+    self.fieldOfView() = 
     
 # def BeePosition(environment, mättnadskänsla, )
 # Semi-slumpmässig rörelse från bi-Kupan
