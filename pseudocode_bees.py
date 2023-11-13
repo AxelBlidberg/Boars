878394750e33e5398b1528f695f151ff9e34b230
Sonja01 <75359906+Sonja01@users.noreply.github.com>
2023-11-13 14:05:17 +0100

    Update pseudocode_bees.py

--- pseudocode_bees.py
+++ pseudocode_bees.py
@@ -1,2 +1,16 @@
-# rörelsen
-# Vilja att besöka blomma, "dragningskraft"
+# Från Miljö-gruppen:
+# class Environment
+#    subclass blommor:  koordinater för olika blommor
+# environment: Vad finns i närheten? (lista)
+
+class Bee:
+  def __init__(environment):
+    self.position() = [0,0,0] # x, y, angle
+    
+    
+# def BeePosition(environment, mättnadskänsla, )
+# Semi-slumpmässig rörelse från bi-Kupan
+  # Vilja att besöka blomma, "dragningskraft" till blommorna alt. synfält beroende på biets riktning
+  # Minne vilken sorts blomma den besökt
+  # Mättnadskänsla
+  # 
