0e7f512e9340461f431685f4b70de9bc17f87b1d
Sonja01 <75359906+Sonja01@users.noreply.github.com>
2023-11-20 14:51:01 +0100

    make to main

--- Environment.py
+++ Environment.py
@@ -1,120 +1,270 @@
 import numpy as np
-import turtle
-import matplotlib.pyplot as plt
+import matplotlib as plt
 
 class Environment:
     def __init__(self, size) -> None:
+        self.size = size
         self.xLimit = size
         self.yLimit = size
         self.flowers = []
+        self.nests = []
         self.hazards = []
-        self.SuperGrid = [] # Added for faster search around location
+        self.iterations = 1
 
-    def CreateGrid():
-        grid = [[[] for _ in range(10)] for i in range(10)]
-        return grid
-
-    def AddFlower(self, n, size):
+    def GetSurroundings(self, position, radius):
         '''
-        Meth
- eht ot srewolf sdda d
+        Returns list sorted on distance on the format: [distance, type, x, y], ex: [1, flower: rose, x, y]
+
+        position = position of the bee
+        radius = How far away to check
+        '''
+        Euclidan = lambda x1, x2: np.sqrt(sum([(x1[i] - x2[i])**2 for i in range(len(x1))]))
+        nearby = []
+        for i in range(len(self.flowers)):
+            x = self.flowers[i].x
+            y = self.flowers[i].y
+            distance = Euclidan(position, [x, y])
+            if distance <= radius:
+                nearby.append([distance, 'flower', x, y])
+
+        nearby = sorted(nearby, key= lambda x: x[0])
+        return nearby
+
+    def InitializeFlowers(self, n):
+        '''
+        Initializes n number of flowers in the environment
+        '''
+        for _ in range(n):
+            center = [self.xLimit/2, self.yLimit/2]
+
+            self.flowers.append(Flower(center, self.xLimit/2, self.iterations, self.xLimit, self.yLimit))
+
+    def AddFlower(self, center, radius, flowerType):
+        '''
+        Method to add one or several flowers to the environment
+
         n = Number of flowers to add
-        
         '''
-        for i in range(n):
-            x = size*np.random.rand()
-            y = size*np.random.rand()
-            self.append(Flower(x, y))
+        self.flowers.append(Flower(center, radius, self.iterations, self.xLimit, self.yLimit, flowerType))
 
-    def AddBeeNest(self):
+    def InitializeBeeNest(self, n):
+        '''
+        Method to initialize n nests in the beginning of the simulation
+        
+        n = Number of nests to add
+        '''
+        center = [self.xLimit/2, self.yLimit/2]
+        for _ in range(n):
+            self.nests.append(Nest(center, self.xLimit/2, self.xLimit, self.yLimit))
+
+    def AddBeeNest(self, n):
+        '''
+        Method to add one or several nests to the environment during the simulation
+
+        n = Number of nests to add
+        '''
         pass
 
-class Flower(Environment):
-    def __init__(self, size, x, y) -> None:
-        super().__init__(size)
+    def ExportContent(self):
+        '''
+        Exports a list of the format [class, 'Type', x, y, object] ex: ['flower', 1, x, y, Flower]
+        '''
+        content = []
 
-        self.x = x
-        self.y = y
-        self.size = size
+        for flower in self.flowers:
+            content.append(['flower', flower.type, flower.x, flower.y, flower])
+
+        for nest in self.nests:
+            content.append(['Nest', 'Nest', nest.x, nest.y, nest])
         
+        return content
+
+    def PushUpdate(self):
+        self.iterations += 1
+        # Update flowers
+        for i, flower in enumerate(self.flowers):
+            status = flower.UpdateFlower(self.iterations)
+            if status[0] == 1:
+                self.AddFlower(status[1], 2)
+            elif status[0] == 2:
+                del self.flowers[i]
+
+
+    def GetObject(self, x, y):
+        '''
+        Returns the object at the specified location for outside manipulation of object, like depositing/ taking pollen.
+
+        x = X coordinate
+        y = Y coordinate
+        type = Which type of object
+        '''
+        content = self.ExportContent()
+        for i in range(len(content)):
+            if content[i][2] == x and content[i][3] == y:
+                return content[i][-1]
+
+
+class Flower:
+    def __init__(self, center, radius, birth, xLimit, yLimit, t='random', color="#fffdff") -> None:
+        # Location
+        self.x = center[0] + radius*np.random.uniform(-1, 1)
+        self.y = center[1] + radius*np.random.uniform(-1, 1)
+        self.location = [self.x, self.y]
+        self.color = color
+        
+        # Type of flower
+        if t == 'random':
+            self.type = np.random.randint(1, 5)
+        else:
+            self.type = t
+            
+        self.flowersize = np.random.randint(1, 5)
         self.nectarAmount = np.random.randint(1,10)
+        self.pollen = {f'{i}': 0 for i in range(1,6)}
+        self.lifespan = 100
+        self.creation = birth
 
-        self.pollen = 0
-        
 
+    def __str__(self) -> str:
+        return f'Flower of type: {self.type} at ({self.x:3.1f}, {self.y:3.1f})'
+
+    def DecreaseNectar(self):
+        '''
+        Decreases the nectar in a flower
+        '''
+        if self.nectarAmount < 0:
+            self.nectarAmount -= 1
     
-    def decreaseNectar(self):
-        self.nectarAmount -= 1
+    def Pollination(self, beeInstance):
+        '''
+        Pollinates flowers depending on the pollen carried by a bee
+        '''
+        for pollenType, amount in beeInstance.pollenCarried.items():
+            self.pollen[pollenType] += amount
 
-    #add Pollen 
+    def UpdateFlower(self, time):
+        '''
+        Update rules for flowers, 
+        '''
+        if self.pollen[f'{self.type}'] >= 10:
+            self.pollen[self.type] -= 10
+            return [1, [self.x, self.y]]
+        elif time - self.creation < self.lifespan:
+            return [2, []]
+        else:
+            return [0, []]
 
-class Hazards(Environment):
-    def __init__(self, size) -> None:
-        super().__init__(size)
 
+class Nest:
+    def __init__(self, center, radius, xLimit, yLimit) -> None:
 
-
-
-#Enkelt bi som rör sig slumpmässigt
-'''
-class Bee:
-    def __init__(self):
-        self.bee = turtle.Turtle()
-        self.bee.shape('triangle')
-        self.bee.color('yellow')
-        self.bee.penup()
-        self.bee.speed(1)
+        # Location of the nest
+        self.x = center[0] + radius*np.random.uniform(-1, 1)
+        if self.x < 0:
+            self.x = 0
+        elif self.x > xLimit:
+            self.x = xLimit
+        self.y = center[1] + radius*np.random.uniform(-1, 1)
+        if self.y < 0:
+            self.y = 0
+        elif self.y > yLimit:
+            self.y = yLimit
+        self.location = [self.x, self.y]
     
-    def moveRight(self):
-        self.bee.setheading(0)
-        self.bee.forward(10)
+
+    def GetLocation(self) -> list:
+        '''
+        Returns the coordinates of a nest
+        '''
+        return [self.x, self.y]
     
-    def moveUp(self):
-        self.bee.setheading(90)
-        self.bee.forward(10)
+    def __str__(self) -> str:
+        return f'Nest at ({self.x}, {self.y})'
 
-    def moveLeft(self):
-        self.bee.setheading(180)
-        self.bee.forward(10)
-    
-    def moveDown(self):
-        self.bee.setheading(270)
-        self.bee.forward(10)
+    def IsOccupied(self, nestInstance) -> bool:
+        '''
+        Checks if a nest is occupied by a bee
+        '''
+        pass
 
-    
-    def randomMove(self):
-        direction = np.random.choice(['right', 'up', 'left', 'down'])
-
-        if direction == 'right':
-            self.moveRight()
-        elif direction == 'up':
-            self.moveUp()
-        elif direction == 'left':
-            self.moveLeft()
-        elif direction == 'down':
-            self.moveDown()
-      
-
-#bee = Bee()
-
-while True:
-    bee.randomMove()
-    turtle.update()
-'''
+    def AssignNest(self, nestInstance, beeInstance):
+        '''
+        Assign a nest to a bee
+        '''
+        if not self.IsOccupied(nestInstance):
+            pass        
 
 
-env = Environment()
-
-env.createGrid()
+class Hazards:
+    def __init__(self) -> None:
+        pass
 
 
 
+def PlotFunction(data, limit):
+    '''
+    Temporary function for plotting the environment. Takes a special formatted list obtained from the ExportContent method in the Environment class.
+    '''
+    # Divide data
+    flowers = []
+    for object in data:
+        if object[0] == 'flower':
+            flowers.append(object)
+
+    nests = []
+    for object in data:
+        if object[0] == 'Nest':
+            nests.append(object)
+
+    types = [item[1] for item in flowers]
+    x_values = [item[2] for item in flowers]
+    x_NestValues = [item[2] for item in nests]
+    y_values = [item[3] for item in flowers]
+    y_NestValues = [item[3] for item in nests]
+
+#     unique_types = set(types)
+#     color_map = {t: i for i, t in enumerate(unique_types)}
+#     colors = [color_map[t] for t in types]
 
 
-env.AddFlower(1, 1)
+    plt.scatter(x_values, y_values, c=colors, cmap='viridis', s=50, alpha=0.8, label=types)
+    plt.scatter(x_NestValues, y_NestValues, marker='^', color='black', label='Black Triangles', s=100)
+    plt.xlabel('X-axis')
+    plt.ylabel('Y-axis')
+    plt.title('Scatter Plot with Colors')
+    plt.xlim([0, limit])
+    plt.ylim([0, limit])
+    plt.show()
 
 
-plt.plot(env.)
+#test = Environment(100)
+#
+#test.InitializeFlowers(5)
+#
+#test.InitializeBeeNest(5)
+#
+#for i in range(30):
+#    obj = np.random.choice(test.flowers)
+#    test.AddFlower(obj.location, 5, obj.type)
+#
+#for i in range(10):
+#    obj = np.random.choice(test.flowers)
+#    C = test.GetObject(obj.x, obj.y)
+#    print(f'Selected flower: {C}')
+#
 
 
 
+##neighbors = test.GetSurroundings([5,5], 5)
+#A = test.ExportContent()
+#B = test.flowers
+##for b in B:
+##    print(b)
+#
+#for a in A:
+#    print(a)
+#test.PushUpdate()
+#
+#
+#PlotFunction(A, 100)
+#
---
+++ Simulation.py
@@ -0,0 +1,123 @@
+import tkinter as tk
+import numpy as np
+
+from tkinter import Scale
+
+from Bee import *
+from Environment import *
+
+class BeeSim(tk.Tk):
+    def __init__(self, size=500, num_bees=1, num_flowers=1):
+        super().__init__()
+        self.size = size
+        self.num_flowers = num_flowers
+       
+
+        self.title("Bee Simulation")
+        
+        self.canvas_frame = tk.Frame(self)
+        self.canvas_frame.pack(side="left", padx=10)
+        self.canvas = tk.Canvas(self.canvas_frame, width=size, height=size, bg='#252526')
+        self.canvas.pack()
+
+        # Frame for sliders
+        self.slider_frame = tk.Frame(self)
+        self.slider_frame.pack(side="right", padx=10)
+
+        # Sliders for controlling parameters
+        self.angular_noise_slider = Scale(self.slider_frame, label="Angular Noise", from_=0.0, to=1.0, resolution=0.01, orient="horizontal", length=200)
+        self.angular_noise_slider.set(0.45)
+        self.angular_noise_slider.pack()
+
+
+        self.vision_range_slider = Scale(self.slider_frame, label="Vision Range", from_=10, to=100, orient="horizontal", length=200)
+        self.vision_range_slider.set(30)
+        self.vision_range_slider.pack()
+
+        self.vision_angle_slider = Scale(self.slider_frame, label="Vision Angle", from_=0, to=359, resolution=1, orient="horizontal", length=200)
+        self.vision_angle_slider.set(280)
+        self.vision_angle_slider.pack()
+
+        self.show_vision_var = tk.BooleanVar(value=True)
+        self.draw_vision_checkbox = tk.Checkbutton(self.slider_frame, text="Draw Vision", variable=self.show_vision_var, onvalue=True, offvalue=False)
+        self.draw_vision_checkbox.pack(pady=5)
+
+        self.nearby = [[0.001, 'right', 0.5, 0.5], [0.003, 'left', -0.5, -0.5]]
+
+        self.environment = Environment(size)
+        self.environment.InitializeFlowers(num_flowers)
+        
+        self.timestep = 0
+        self.max_age = 500 # in timesteps
+
+        self.bees = [Bee(np.random.uniform(10, size-10), np.random.uniform(10, size-10),self.timestep) for _ in range(num_bees)]
+
+        self.after(50, self.UpdateModel)
+
+    def DrawEnvironment(self):
+        size = 4
+        for flower in self.environment.flowers:
+            x, y = flower.x, flower.y
+            self.canvas.create_oval(x - size, y - size, x + size, y + size, fill=flower.color)
+    
+    def DrawBee(self, bee):
+        x, y = bee.x, bee.y
+        self.canvas.create_oval(x - 5, y - 5, x + 5, y + 5, fill=bee.color)
+
+    def DrawVisionField(self, bee):
+        start_angle = (np.degrees(bee.orientation) - bee.vision_angle / 2) % 360
+        extent = bee.vision_angle
+        if start_angle > 0:
+            start_angle += extent
+
+        # Create the arc
+        self.canvas.create_arc(bee.x - bee.vision_range, bee.y - bee.vision_range, bee.x + bee.vision_range, bee.y + bee.vision_range,
+                            start=-start_angle, extent=extent, outline='lightblue', width=1)
+
+    def DrawPath(self, bee):
+        if bee.path:
+            self.canvas.create_line(bee.path, fill='#ffea61', width=1)
+    
+    def CheckBoundaryCollision(self, bee): # fastnar på blommor utanför canvas fixas nog bäst i Environment
+        if 0+5 < bee.x < self.size-5 and 0+5 < bee.y < self.size-5:
+            return
+        bee.orientation += np.pi/2
+
+    def UpdateModel(self):
+        self.canvas.delete('all')
+        self.timestep += 1
+
+        angular_noise = float(self.angular_noise_slider.get())
+        vision_range = int(self.vision_range_slider.get())
+        vision_angle = float(self.vision_angle_slider.get())
+         
+        # new bees
+        if self.timestep % 100==1: # change to pollen-related, and so new bees are born in nests?
+            self.bees.append(Bee(np.random.uniform(10, self.size-10), np.random.uniform(10, self.size-10),self.timestep))
+
+        for bee in self.bees:
+
+            # kill bee if old
+            if self.timestep - bee.birth > self.max_age:
+                del bee
+                continue
+
+            bee.angular_noise, bee.vision_range, bee.vision_angle = angular_noise, vision_range, vision_angle
+            
+            bee.Update(self.environment. flowers)
+            self.CheckBoundaryCollision(bee)
+            self.DrawBee(bee)
+            self.DrawPath(bee)
+
+            if self.show_vision_var.get():
+                self.DrawVisionField(bee)  
+        
+        self.DrawEnvironment()
+      
+        self.after(50, self.UpdateModel)
+
+
+    
+if __name__ == "__main__":
+    bee_sim = BeeSim(size= 800, num_bees=5, num_flowers=150)
+    bee_sim.mainloop()
