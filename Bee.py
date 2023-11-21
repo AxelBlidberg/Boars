b4545e69875b80c603f2377cbe934b05b8bf7830
Sonja01 <75359906+Sonja01@users.noreply.github.com>
2023-11-21 22:48:43 +0100

    bees eat and starve, return home + leave pollen

--- Bee.py
+++ Bee.py
@@ -1,9 +1,10 @@
 import numpy as np
 
 class Bee:
-    def __init__(self, x, y, birth, vision_angle=180, vision_range=40, angular_noise=0.01, speed=2, color="#ffd662"):
-        self.x = x
-        self.y = y
+    def __init__(self, nest, birth, pollen={},vision_angle=180, vision_range=40, angular_noise=0.01, speed=2, color="#ffd662"):
+        self.x = nest.x
+        self.y = nest.y
+        self.home = nest    # (object)
         self.path = [[self.x, self.y]]
         self.path_length = 100
 
@@ -20,10 +21,14 @@ class Bee:
         self.vision_range = vision_range
 
         self.nectar = 0         # 0=hungry, 1 = fed?
-        self.pollen = {}        # how much pollen and what kind
+        self.pollen = pollen        # how much pollen and what kind
 
         self.color = color
         self.birth = birth
+
+        bee_age_mean = 500
+        self.max_age = np.random.normal(loc=bee_age_mean, scale=50,size=1)[0] # each individual has "random" life-length
+
 
     def Update(self, flowers):
         # Angular noise to the direction
@@ -32,7 +37,7 @@ class Bee:
         # Get nearby flowers within the field of view and range excluding visited.
         nearby_flowers = [flower for flower in flowers if self.InFieldOfView(flower)]
         nearby_flowers = [flower for flower in nearby_flowers if flower not in self.visited_flowers]
-        
+
         if nearby_flowers:
             nearest_flower = min(nearby_flowers, key=lambda flower: np.linalg.norm([flower.x - self.x, flower.y - self.y]))
             direction_to_nearest = np.array([nearest_flower.x - self.x, nearest_flower.y - self.y])
@@ -44,17 +49,18 @@ class Bee:
                 self.visited_flowers.append(nearest_flower)
                 flowerType = nearest_flower.type 
                 
-                pollen_taken = np.random.randint(0, nearest_flower.pollen)
+                pollen_taken = np.random.randint(1, nearest_flower.pollen) # changed to min=1 to avoid negative food later
 
                 if flowerType in self.pollen.keys():
                     
                     r = np.random.random()
 
                     if r < 1: #dummy value TODO: Fixa varierande reproduktion för varje blomma
-
-                        nearest_flower.pollen += self.pollen[flowerType] #Allt pollen ges från biet till blomman om pollen sker just nu
-                        self.pollen[flowerType] = 0
-
+                        pollen_given = 10
+                        nearest_flower.pollen += pollen_given #self.pollen[flowerType] #Allt pollen ges från biet till blomman om pollen sker just nu
+                        self.pollen[flowerType] -= pollen_given # bytte till 10 temporärt för att undvika problem med food / sonja
+                        if self.pollen[flowerType] < 1: # remove key if no pollen, so it cant get negative
+                            self.pollen.pop(flowerType)         
                     else:
                         self.pollen[flowerType] += pollen_taken #Biet tar en viss mängd pollen
                         nearest_flower.pollen -= pollen_taken
@@ -82,9 +88,18 @@ class Bee:
                 if len(self.visited_flowers) > self.short_memory:
                     self.visited_flowers.pop(0)
                 
+                          
+                
         else:
             self.orientation = self.orientation + self.angular_noise * W 
         
+        # eats x random pollen each timestep
+        if len(self.pollen) > 0:
+            eating_pase = 1          # pollen eaten per timestep
+            random_pollen_key = list(self.pollen.keys())[np.random.randint(0,len(self.pollen))]
+            self.pollen[random_pollen_key] -= eating_pase   
+            if self.pollen[random_pollen_key] < 1: # remove key if no pollen, so it cant get negative
+                self.pollen.pop(random_pollen_key) 
 
         self.x += self.speed * np.cos(self.orientation)
         self.y += self.speed * np.sin(self.orientation)
@@ -93,7 +108,43 @@ class Bee:
 
         if len(self.path) > self.path_length:
             self.path.pop(0)
+
+
+    def ReturnHome(self):
+        nearby_home = self.home if self.InFieldOfView(self.home) else False
+        if nearby_home:
+            distance_to_home = np.linalg.norm([self.home.x - self.x, self.home.y - self.y])
+            if distance_to_home <= self.visit_radius:
+                print('bee went home to leave pollen. Pollen levels before:',self.pollen)
+                print('Nest pollen level:',self.home.pollen)
+                leave_home_ratio = 1/2
+                food = sum(self.pollen.values())
+                for key in self.pollen.keys(): # leave the same ratio of each pollen type
+                    self.pollen[key] = int(self.pollen[key] * (1-leave_home_ratio)) # bee loses pollen
+                pollen_given = int(food * leave_home_ratio)
+                self.home.pollen += pollen_given
+                print('Pollen after',self.pollen)
+                print('Nest pollen level:',self.home.pollen)
         
+        W = np.random.uniform(-1/2, 1/2)
+        direction_to_home = np.array([self.home.x - self.x, self.home.y - self.y])
+        self.orientation = np.arctan2(direction_to_home[1], direction_to_home[0]) + self.angular_noise * W
+
+         # eats x random pollen each timestep
+        if len(self.pollen) > 0:
+            eating_pase = 1          # pollen eaten per timestep
+            random_pollen_key = list(self.pollen.keys())[np.random.randint(0,len(self.pollen))]
+            self.pollen[random_pollen_key] -= eating_pase   
+            if self.pollen[random_pollen_key] < 1: # remove key if no pollen, so it cant get negative
+                self.pollen.pop(random_pollen_key) 
+
+        self.x += self.speed * np.cos(self.orientation)
+        self.y += self.speed * np.sin(self.orientation)
+        self.velocity = [self.speed * np.cos(self.orientation), self.speed * np.sin(self.orientation)]
+        self.path.append([self.x, self.y])
+
+        if len(self.path) > self.path_length:
+            self.path.pop(0)
 
     def InFieldOfView(self, obj):
         direction_vector = np.array([obj.x - self.x, obj.y - self.y])
--- Simulation.py
+++ Simulation.py
@@ -11,7 +11,8 @@ class BeeSim(tk.Tk):
         super().__init__()
         self.size = size
         self.num_flowers = num_flowers
-       
+        
+        self.timestep = 0
 
         self.title("Bee Simulation")
         
@@ -42,17 +43,14 @@ class BeeSim(tk.Tk):
         self.draw_vision_checkbox = tk.Checkbutton(self.slider_frame, text="Draw Vision", variable=self.show_vision_var, onvalue=True, offvalue=False)
         self.draw_vision_checkbox.pack(pady=5)
 
-        self.nearby = [[0.001, 'right', 0.5, 0.5], [0.003, 'left', -0.5, -0.5]]
-
         self.environment = Environment(size)
-        self.environment.InitializeFlowers(num_flowers)
+        self.environment.InitializeFlowers(num_flowers,self.timestep)
         self.environment.InitializeBeeNest(num_bees)
         
-        self.timestep = 0
 
-        self.max_age = 500 # bees max age, in timesteps
-        ages_first_bees = np.random.normal(loc=-80, scale= 20, size=num_bees) # random birth-dates on first bees
-        self.bees = [Bee(self.environment.nests[i].x, self.environment.nests[i].y, ages_first_bees[i]) for i in range(num_bees)]
+        ages_first_bees = np.random.randint(-200, 0, size=num_bees) # random birth-dates on first bees
+        pollen_first_bees = [abs(age) for age in ages_first_bees] # so first bees that are old don't starve immediately
+        self.bees = [Bee(self.environment.nests[i], ages_first_bees[i],{1:pollen_first_bees[i]}) for i in range(num_bees)]
 
         self.after(50, self.UpdateModel)
 
@@ -71,8 +69,9 @@ class BeeSim(tk.Tk):
         for nest in self.environment.nests:
             x, y = nest.x, nest.y
             self.canvas.create_rectangle(x - nest_size, y - nest_size, x + nest_size, y + nest_size, fill=nest.color)
-
+        
         self.environment.PushUpdate(self.timestep)
+
 
     def DrawBee(self, bee):
         x, y = bee.x, bee.y
@@ -105,28 +104,39 @@ class BeeSim(tk.Tk):
         vision_range = int(self.vision_range_slider.get())
         vision_angle = float(self.vision_angle_slider.get())
         
-        
-        
         self.DrawEnvironment() 
 
-
         # new bees
-        
-        if self.timestep % 100==0: # change to pollen-related, and so new bees are born in nests?
+        if self.timestep % 50==0: # change to pollen-related
             nest = self.environment.nests[np.random.randint(len(self.environment.nests))] # born in random nest
-            self.bees.append(Bee(nest.x, nest.y, self.timestep))
+            self.bees.append(Bee(nest, self.timestep))
         
-        for bee in self.bees:
+        for bee_number, bee in enumerate(self.bees):
+            
+            bee.angular_noise, bee.vision_range, bee.vision_angle = angular_noise, vision_range, vision_angle
 
             # kill bee if old
-            
-            if self.timestep - bee.birth > self.max_age:
+            bee_age = self.timestep - bee.birth
+            if  bee_age > bee.max_age: 
+                print('RIP: bee died of age:',bee_age,'. Pollen levels:',bee.pollen)
+                self.bees.pop(bee_number)
                 del bee
                 continue
             
-            bee.angular_noise, bee.vision_range, bee.vision_angle = angular_noise, vision_range, vision_angle
+            # kill bee if starving
+            food = sum(bee.pollen.values())
+            if food < 1 and bee_age > 100:  # make pollen non-empty for first bees?
+                print('RIP: bee died of starvation. Age:',bee_age,'. Pollen levels:',bee.pollen)
+                self.bees.pop(bee_number)
+                del bee
+                continue
+            full = 200
+            if food > full:
+                bee.ReturnHome() # return to home nest if full
+            else:
+                bee.Update(self.environment.flowers)
             
-            bee.Update(self.environment. flowers)
+            
             self.CheckBoundaryCollision(bee)
             self.DrawBee(bee)
             self.DrawPath(bee)
@@ -134,7 +144,8 @@ class BeeSim(tk.Tk):
             if self.show_vision_var.get():
                 self.DrawVisionField(bee)  
         
-        
+        if self.timestep%20 == 1:
+            self.environment.InitializeFlowers(1, self.timestep)
       
         self.after(50, self.UpdateModel)
 
