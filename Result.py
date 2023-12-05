867baa43560f0b75d1993b7098360085b458a0fe
Oscarrosman <127534362+Oscarrosman@users.noreply.github.com>
2023-12-05 13:34:25 +0100

    Update Result.py

--- Result.py
+++ Result.py
@@ -1,7 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from collections import Counter
-from devFile import *
 
 def PlotFlowerAmount(ax1, ax2, fData):
     trends = [[], [], [], [], []]
