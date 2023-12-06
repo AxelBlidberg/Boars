07658bf448a6ccade97d1f5900cb32b4f44fcb39
AxelBlidberg <98399838+AxelBlidberg@users.noreply.github.com>
2023-12-06 11:30:49 +0100

    cluster plots working

---
+++ standaloneSimulation.py
@@ -0,0 +1,43 @@
+
+from Bee import *
+from Environment import *
+from Result import *
+
+class BeeSimulation():
+    def __init__(self, size, numStartingBees, numStartingFlowers, seasonLength, envType='countryside') -> None:
+        self.size = size
+        self.numStartingBees = numStartingBees
+        self.numStartingFlowers = numStartingFlowers
+        self.seasonLength = seasonLength
+        self.timestep = 0
+        self.season = 1
+
+        self.environment = Environment(size,self.seasonLength, envType)
+        self.environment.InitializeFlowers(numStartingFlowers)
+        self.environment.InitializeBeeNest(numStartingBees)
+
+        self.swarm = Swarm(self.seasonLength)
+    
+        self.swarm.InitializeBees(numStartingBees, self.environment.nests)
+
+    def CheckBoundaryCollision(self, bee): 
+        if 0+5 < bee.x < self.size-5 and 0+5 < bee.y < self.size-5:
+            return
+        bee.orientation += np.pi/3
+
+    def Update(self):
+        self.timestep += 1 
+        self.swarm.PushUpdate(self.environment.flowers, self.timestep)
+
+        if self.timestep % self.seasonLength ==0 and self.timestep>0:
+            self.season += 1
+
+            self.environment.newNests = self.swarm.newNests
+            self.environment.CreateNewGeneration(self.timestep)
+            self.swarm.CreateNewGeneration(self.timestep, self.environment.nests)
+
+        if len(self.swarm.bees) == 0: # Jump in time if no bees
+            self.timestep = (self.season+1) * self.seasonLength 
+             
+        for bee in self.swarm.activeBees:
+            self.CheckBoundaryCollision(bee)
