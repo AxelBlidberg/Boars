0d0241927e5552bc7585236435fe7983fe5c48ab
AxelBlidberg <axelblidberg@gmail.com>
2023-12-10 20:55:53 +0100

    Update

--- clusterPlots.py
+++ clusterPlots.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.spatial import Voronoi, voronoi_plot_2d
 
-from Simulation import *
+from standaloneSimulation import *
 
 #Definition av rum
 #Grid på 1000 x 1000
@@ -13,14 +13,15 @@ from Simulation import *
 #Vi räknar med att ett bi max lever 8 veckor
 #En dag är 2000 tidssteg
 
-def run_model(size, num_bees=1, num_flowers=20, envType='countryside', visualize=False, NumSeason=1, seasonLength=100, iterations=10):
+def run_model(size, num_bees=1, num_flowers=20, envType='countryside', NumSeason=1, seasonLength=100, iterations=10):
 
-    beesim = BeeSim(size, num_bees, num_flowers, envType, visualize, NumSeason, seasonLength)
+    beesim = BeeSim(size, num_bees, num_flowers, envType, NumSeason, seasonLength)
 
     clustering_coefficients = []
     min_radius = 10
     vor = None
     init_positions = np.array([[flower.x, flower.y] for flower in beesim.environment.flowers])
+    init_nests = np.array([[nest.x, nest.y] for nest in beesim.environment.nests])
     #vor_init = Voronoi(positions)
     #for _ in range(iterations):
        # beesim.Update()
@@ -29,12 +30,13 @@ def run_model(size, num_bees=1, num_flowers=20, envType='countryside', visualize
         #     vor = Voronoi(positions)
         #     clustering_coefficients.append(clustering(vor, len(beesim.environment.flowers), min_radius))
     beesim.RunSimulation()
-    positions = [[flower.x, flower.y] for flower in beesim.environment.flowers]
+    positions = np.array([[flower.x, flower.y] for flower in beesim.environment.flowers])
+    nests = np.array([[nest.x, nest.y] for nest in beesim.environment.nests])
 
     
     #vor = Voronoi(positions)
         
-    return init_positions, positions #vor_init, vor, clustering_coefficients
+    return init_positions, positions, init_nests, nests #vor_init, vor, clustering_coefficients
         
 def clustering(vor, N, min_radius):
     valid_count = 0
@@ -48,34 +50,39 @@ def clustering(vor, N, min_radius):
     clustering_coefficient = valid_count / N
     return clustering_coefficient
 
-def plot_clustering(size, num_bees=1, num_flowers=20, envType='countryside', visualize=False, NumSeason=1, seasonLength=100, iterations=10):
+def plot_clustering(size, num_bees=1, num_flowers=20, envType='countryside', NumSeason=1, seasonLength=100, iterations=10):
 
    # vor_init, vor, clustering_coefficients = run_model(size, num_bees, num_flowers, envType, visualize, NumSeason, seasonLength, iterations)
 
-    init_positions, positions = run_model(size, num_bees, num_flowers, envType, visualize, NumSeason, seasonLength, iterations)
+    init_positions, positions, init_nests, nests = run_model(size, num_bees, num_flowers, envType, NumSeason, seasonLength, iterations)
 
     fig, axes = plt.subplots(nrows=1, ncols=3, figsize=(15, 5))
 
     # Plot Voronoi plot in the first column
     #voronoi_plot_2d(vor_init, ax=axes[0], show_vertices=False, line_colors='lightblue', line_width=2, line_alpha=0.6, point_size=8)
-    
+    #print(f"init_positions: {init_positions}")
+    #print(f"positions: {positions}")
+
     axes[0].scatter(init_positions[:,0], init_positions[:,1], marker='.') 
+    axes[0].scatter(init_nests[:,0], init_nests[:,1], marker='s', s=25) 
     axes[0].set_title('Initial configuration') 
     axes[0].set_xlim(0, size) 
     axes[0].set_ylim(0, size) 
+    axes[0].set_xticks([])
+    axes[0].set_yticks([])
     
     # Plot Voronoi plot in the second column
     #voronoi_plot_2d(vor, ax=axes[1], show_vertices=False, line_colors='lightblue', line_width=2, line_alpha=0.6, point_size=8)
     #axes[1].plot(positions)
-    if len(positions)>1:
-        axes[1].scatter(positions[:,0], positions[:,1], marker='.')
-    else:
-        print("no final flowers")
-        print(positions)
-
+    axes[1].scatter(positions[:,0], positions[:,1], marker='.') if len(positions)>1 else print("no final flowers")
+    
+    axes[1].scatter(nests[:,0], nests[:,1], marker='s', s=25) if len(nests>1) else print("no final nests")
+    
     axes[1].set_title('Final configuration')
     axes[1].set_xlim(0, size)  
     axes[1].set_ylim(0, size)    
+    axes[1].set_xticks([])
+    axes[1].set_yticks([])
 
     #axes[2].plot(clustering_coefficients[:], label='Clustering')
     #axes[2].legend()
@@ -91,4 +98,4 @@ numStartingBees = 10
 NumSeason = 2
 
 iterations = (seasonLength)*NumSeason
-plot_clustering(size=1000, num_bees=numStartingBees, num_flowers=numStartingFlowers, envType='countryside', visualize=None, NumSeason=NumSeason, seasonLength=seasonLength, iterations=iterations)
+plot_clustering(size=1000, num_bees=numStartingBees, num_flowers=numStartingFlowers, envType='countryside', NumSeason=NumSeason, seasonLength=seasonLength, iterations=iterations)
--- standaloneSimulation.py
+++ standaloneSimulation.py
@@ -1,46 +1,76 @@
-
+import numpy as np
+from tqdm import tqdm
 from Bee import *
 from Environment import *
 from Result import *
+import time
 
-class BeeSimulation():
-    def __init__(self, size, numStartingBees, numStartingFlowers, seasonLength, envType='countryside') -> None:
+class BeeSim:
+    def __init__(self, size=1000, num_bees=4, num_flowers=1500, envType='countryside', NumSeason=10, seasonLength=5000):
+        # Initialize simulation parameters
         self.size = size
-        self.numStartingBees = numStartingBees
-        self.numStartingFlowers = numStartingFlowers
+        self.num_flowers = num_flowers
         self.seasonLength = seasonLength
-        self.timestep = 1
+        self.timestep = 0
         self.season = 1
+        self.simulationLength = NumSeason
+        self.SimulationBar = tqdm(total=self.simulationLength * self.seasonLength, desc="Simulation progress", unit=" Time steps")
+        self.seasonStartTime = time.time()
 
-        self.environment = Environment(size,self.seasonLength, envType)
-        self.environment.InitializeFlowers(numStartingFlowers)
-        self.environment.InitializeBeeNest(numStartingBees)
-
-        self.swarm = Swarm(self.seasonLength)
-    
-        self.swarm.InitializeBees(numStartingBees, self.environment.nests)
-
-    def CheckBoundaryCollision(self, bee): 
-        if 0+5 < bee.x < self.size-5 and 0+5 < bee.y < self.size-5:
-            return
-        bee.orientation += np.pi/3
-
-    def Update(self):
-        self.timestep += 1 
-        self.swarm.PushUpdate(self.environment.flowers, self.timestep)
-        self.environment.PushUpdate(self.timestep)
+        # Initialize environment and swarm
+        self.environment = Environment(size, self.seasonLength, envType)
+        self.environment.InitializeFlowers(num_flowers)
+        self.environment.InitializeBeeNest(num_bees)
         
-        if self.timestep % self.seasonLength ==0:
-            self.season += 1
-            print(f"season {self.season}")
-
+        self.swarm = Swarm(self.seasonLength)
+        self.swarm.InitializeBees(num_bees, self.environment.nests, self.timestep)
+        
+    def Update(self):
+        self.timestep += 1
+        if self.timestep % self.seasonLength == 0:
+            self.SimulationBar.write(f'Time to simulate season {self.season}: {(time.time() - self.seasonStartTime)//60:2.0f} minutes and {(time.time() - self.seasonStartTime)%60:2.0f} seconds')
+            self.seasonStartTime = time.time()
             self.environment.newNests = self.swarm.newNests
             self.environment.CreateNewGeneration(self.timestep)
             self.swarm.CreateNewGeneration(self.timestep, self.environment.nests)
+            self.season += 1
 
-        if len(self.swarm.bees) == 0: # Jump in time if no bees
-            self.timestep = (self.season+1) * self.seasonLength 
-            print(f"season{self.season}")
-             
-        for bee in self.swarm.activeBees:
-            self.CheckBoundaryCollision(bee)
+        self.swarm.PushUpdate(self.environment.flowers, self.timestep)
+        self.environment.PushUpdate(self.timestep)
+        self.SimulationBar.update(1)
+
+    def RunQuarter(self, quarter):
+        # Simulate one quarter of the season
+        for _ in range(int(0.25 * self.seasonLength)):
+            self.timestep += 1
+            self.swarm.PushUpdate(self.environment.flowers, self.timestep)
+            self.environment.PushUpdate(self.timestep)
+            self.SimulationBar.update(1)
+
+    def RunSimulation(self):
+        quarters = [1, 2, 3, 4]
+        startTime = time.time()
+
+        for season in range(self.simulationLength):
+            seasonStartTime = time.time()
+            # Create new generation
+            if season > 0:
+                self.environment.newNests = self.swarm.newNests
+                self.environment.CreateNewGeneration(self.timestep)
+                self.swarm.CreateNewGeneration(self.timestep, self.environment.nests)
+
+            for quarter in quarters:
+                # Run each quarter
+                self.RunQuarter(quarter)
+                #SimulationBar.update(self.seasonLength/4)
+                
+            self.season += 1
+            self.SimulationBar.write(f'Time to simulate season {self.season}: {(time.time() - seasonStartTime)//60:2.0f} minutes and {(time.time() - seasonStartTime)%60:2.0f} seconds')
+
+        self.SimulationBar.close()
+        print(f'Simulation time: {(time.time() - startTime)//60:2.0f} minutes and {(time.time() - startTime)%60:2.0f} seconds')
+
+if __name__ == "__main__":
+    # Example usage
+    simulation = BeeSim()
+    simulation.RunSimulation()
